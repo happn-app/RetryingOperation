89c453b7e31d8a7d1812ca14d95610bf02c6752b
Frizlab <fload@me.com>
2020-07-09 13:38:06 +0200

    Add specific iOS 8 compatibility for the project

--- Package.swift
+++ Package.swift
@@ -4,6 +4,12 @@ import PackageDescription
 
 let package = Package(
 	name: "RetryingOperation",
+	platforms: [
+		.macOS(.v10_10),
+		.iOS(.v8),
+		.tvOS(.v9),
+		.watchOS(.v2)
+	],
 	products: [
 		.library(name: "RetryingOperation", targets: ["RetryingOperation"]),
 	],
